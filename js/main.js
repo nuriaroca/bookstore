f46e4abeb0b1f65cf35a9beca22b289de96dc550
Núria Roca <n.roca.ubiqum@gmail.com>
2019-06-28 10:25:48 +0200

    all with vue

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+Vue.component('book', {
+    props: ['book'],
+    template: ' <div class="flip-card"><div class="flip-card-inner"><div class="flip-card-front"><img v-bind:src="book.portada" class="smallImg"></div><div class="flip-card-back"><h2>{{book.titulo}}</h2><p class="info">{{book.descripcion}}</p><a v-bind:href="book.detalle" data-fancybox="galery"><button type="button" class="btn btn-info">Más info</button></a></div></div></div>'
+
+})
+
 var myVue = new Vue({
     el: "#app",
     data: {
+        search: '',
         books: []
     },
     created() {
         console.log("done with vue.js...")
-        this.megafuncion()
+        this.getData()
     },
     methods: {
-        megafuncion() {
+        getData() {
             fetch("https://api.myjson.com/bins/1h3vb3")
                 .then(function (response) {
                     if (response.ok) {
@@ -18,34 +25,34 @@ var myVue = new Vue({
                 }).then(function (json) {
                     myVue.books = json.books;
 
-                    console.log(myVue.books)
-
-                    // megafuncion(books);
-
                 }).catch(function (error) {
                     console.log("Request failed: " + error.message);
                 });;
         },
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter(book => {
+                return book.titulo.toUpperCase().includes(this.search.toUpperCase())
+            })
+        }
     }
 })
 
-
-
-
-// // SEARCH
+// SEARCH
 // function myFunction() {
 //     var searchValue, allBooks;
 
-//     allBooks = document.getElementsByClassName("divBook");
+//     allBooks = document.getElementsByClassName("flip-card");
 //     searchValue = document.getElementById("myInput").value.toUpperCase();
 
 //     for (i = 0; i < allBooks.length; i++) {
-//         var bookText = allBooks[i].innerText.toUpperCase(); //tot el text de cada llibre en mayuscules
+//         var bookText = allBooks[i].innerText.toUpperCase();
 
 //         if (bookText.includes(searchValue)) {
-//             allBooks[i].style.display = ""; //pot set he de mostrar tot el box, no només h2
+//             allBooks[i].style.display = "";
 //         } else {
-//             allBooks[i].style.display = "none"; //amagar tot el box, no només h2????
+//             allBooks[i].style.display = "none";
 //         }
 //     }
 // }
